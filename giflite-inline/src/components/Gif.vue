804eeff0e0b7f9c0f86a99f3bce629a3092a4571
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2019-03-26 01:22:52 +0430

    feat(giflite-inline): Add loading

--- giflite-inline/src/components/Gif.vue
+++ giflite-inline/src/components/Gif.vue
@@ -1,36 +1,44 @@
 <template>
   <div class="gif-card">
-    <div class="containter">
-      <div class="save">
-        <div class="img_overlay"/>
+      <template v-if="!!gifurl">
+        <img :style="style" v-on:click="toggleShow" class="gif" :src="gifurl" alt="image place">
         <i class="fa fa-bookmark save-btn" v-on:click="addToFavourites"/>
-      </div>
-      <img :style="style" v-on:click="toggleShow" class="gif" :src="gifurl" alt="image place">
+      </template>
+      <Loading v-else />
     </div>
-  </div>
 </template>
 
 <script>
+import Loading from './Loading'
 import { calculateScales } from './../helper/helperFunctions'
 const R = require('ramda')
+
 export default {
   name: 'Gif',
+
   data() {
     return {
       gifurl: '',
-      play: true,
+      play: true, 
     }
   },
+
+  components: {
+    Loading,
+  },
+
   props: {
     gifInfos: Object,
     addToFavourites: Function,
   },
+
   computed: {
     style() {
       const { width } = this.gifInfos
       return `width: ${calculateScales({ width }) * width}px;`
     },
   },
+
   watch: {
     gifInfos() {
       this.gifurl = `https://giflite.herokuapp.com/load/content?url=${
@@ -38,6 +46,7 @@ export default {
       }`
     },
   },
+
   methods: {
     toggleShow() {
       this.play = !this.play
@@ -50,13 +59,9 @@ export default {
 </script>
 
 <style scoped>
-.containter {
-  display: flex;
-  position: relative;
-}
-
 .gif-card {
   width: 350px;
+  height: 200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -64,19 +69,22 @@ export default {
   position: relative;
   background-color: black;
 }
+
 .save-btn {
   background-color: #2b303b;
-  position: absolute;
+  position: fixed;
   box-sizing: content-box;
-  bottom: 1px;
-  left: 2px;
+  top: 165px;
+  left: 10px;
   border: 5px solid #2b303b;
   border-radius: 8px;
   color: #4f5b66;
 }
+
 .save-btn:active {
   opacity: 0.6;
 }
+
 .gif {
   box-sizing: content-box;
 }
