28bf94bb5810af7fc1cf5842c0229fbeb5063097
javad <vahedi.r46@gmail.com>
2019-04-28 23:29:50 -0700

    fix(fix): herokus are removed from config file

--- giflite-inline/src/config.js
+++ giflite-inline/src/config.js
@@ -1,9 +1,7 @@
 export default (process.env.NODE_ENV === 'development'
   ? {
-    server: 'https://giflite.herokuapp.com',
-    // server: 'http://localhost:4180',
+    server: 'http://localhost:4180',
   }
   : {
     server: 'https://wapp.weblite.me/giflite',
-    // server: 'https://giflite.herokuapp.com',
   })
