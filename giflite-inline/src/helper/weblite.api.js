ee42ab53b519f6919326ff2dc6f201e636461fb1
javad <vahedi.r46@gmail.com>
2019-05-18 04:02:22 -0700

    fix-load : some bugs fixed

--- giflite-inline/src/helper/weblite.api.js
+++ giflite-inline/src/helper/weblite.api.js
@@ -4,6 +4,7 @@ const { W } = window;
 export default vueRoot => {
   /* Load Data */
   W.loadData().then(({ user, customize: { id, gifId } }) => {
+
     vueRoot.userId = user.id;
     if (id) vueRoot.gifId = id
     if (gifId) vueRoot.gifId = gifId
